033e7e73b40a2b748ef1c34cc482e6e67b290f5f
t-ninkovic <tijana.ninkovic6@gmail.com>
2017-04-01 13:22:18 +0200

    Implementirana drugacija metoda dodajKurs na master grani

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,16 +1,26 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import menjacnicainterfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
+	LinkedList<Valuta> listaKurseva = new LinkedList<Valuta>();
 
 	@Override
 	public void dodajKurs(String naziv, String skraceni, double prodajni, double kupovni, double srednji,
 			GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-
+		if (naziv == null || skraceni == null || prodajni <= 0 || kupovni <= 0 || srednji <= 0 || datum == null)
+			throw new RuntimeException("Neki od parametara nije ispravno unet");
+		Valuta v = new Valuta();
+		v.setNaziv(naziv);
+		v.setSkraceni(skraceni);
+		v.setProdajni(prodajni);
+		v.setKupovni(kupovni);
+		v.setSrednji(srednji);
+		v.setDatum(datum);
+		listaKurseva.add(v);
 	}
 
 	@Override
